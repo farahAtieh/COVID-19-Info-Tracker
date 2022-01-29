b0e53377651cf5fc38e1f87d022cf2e3d70808e6
FarahAtieh <farahatieh96@gmail.com>
2022-01-29 17:01:33 +0200

    covid-19 info tracker app implementation

--- app/src/main/java/com/example/covid_19infotracker/presentation/splash/view/SplashFragment.kt
+++ app/src/main/java/com/example/covid_19infotracker/presentation/splash/view/SplashFragment.kt
@@ -3,6 +3,5 @@ package com.example.covid_19infotracker.presentation.splash.view
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 
-
 class SplashFragment: Fragment() {
 }
